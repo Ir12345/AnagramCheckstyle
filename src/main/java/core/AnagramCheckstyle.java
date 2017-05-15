4afd621644b4ab9172f4dfeb2d94b042d9746aea
246_135 <My_email_adress@gmail.com>
2017-05-15 14:48:02 -0700

    Changed class AnagramCheckstyle 6

--- src/main/java/core/AnagramCheckstyle.java
+++ src/main/java/core/AnagramCheckstyle.java
@@ -1,13 +1,12 @@
 package core;
 
-
 public class AnagramCheckstyle {
-	/** {@inheritDoc} */
+
 	public static boolean validate(String s, String t) {
         if (s.replaceAll("\\s", "").length() != t.replaceAll("\\s", "").length()) {
 
         return false;}
-        
+
          int value = 0;
          for (int i = 0; i < s.replaceAll("\\s", "").length(); i++) {
 
@@ -18,9 +17,9 @@ public class AnagramCheckstyle {
          return value == 0;
   }
 	
-	public static void main(String[] args) 
+	public static void main(String[] args) {
 
-	    { System.out.println("Debit Card and Bad Credit is anagram? - " + AnagramCheckstyle.validate("Debit Card", "Bad Credit"));
+	     System.out.println("Debit Card and Bad Credit is anagram? - " + AnagramCheckstyle.validate("Debit Card", "Bad Credit"));
 	    System.out.println("Охренеть and Не хрена себе is anagram? - " + AnagramCheckstyle.validate("Охренеть", "Не хрена себе"));
 	       }
 }
